a34de5f367acd3bf24797d7d37a9ebb8f476d582
Mendrika Ramilison <mendrika@wylog.com>
2025-05-13 23:40:56 +0300

    add github, email, linkedin icon

--- portfolio/src/Header.js
+++ portfolio/src/Header.js
@@ -1,3 +1,6 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+
 export default function Header({ setCurrentSection }) {
   return (
     <header>
@@ -7,6 +10,22 @@ export default function Header({ setCurrentSection }) {
         <button onClick={() => setCurrentSection("projets")}>Projets</button>
         <button onClick={() => setCurrentSection("parcours")}>Parcours</button>
         <button onClick={() => setCurrentSection("contact")}>Me contacter</button>
+
+        <div style={{ display: "flex", gap: "16px", fontSize: "24px" }}>
+            <a href="https://github.com/votre-profil" target="_blank" rel="noopener noreferrer">
+                <FaGithub color="#181717" />
+            </a>
+
+            
+            <a href="mailto:votre@email.com">
+                <MdEmail color="#D44638" />
+            </a>
+
+            
+            <a href="https://linkedin.com/in/votre-profil" target="_blank" rel="noopener noreferrer">
+                <FaLinkedin color="#0A66C2" />
+            </a>
+        </div>
       </nav>
     </header>
   );
