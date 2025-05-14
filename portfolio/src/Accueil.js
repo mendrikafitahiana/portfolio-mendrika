0d8343fa0e91acfa44226422bef6a90c51b7a938
Mendrika Ramilison <mendrika@wylog.com>
2025-05-14 19:26:37 +0300

    contenu accueil

--- portfolio/src/Accueil.js
+++ portfolio/src/Accueil.js
@@ -3,6 +3,18 @@ export default function Accueil() {
     <section className="home">
       <h1>RAMILISON Solofoniaina Mendrika Fitahiana</h1>
       <h3>Développeur web fullstack et mobile</h3>
+
+      <p>Développeur Fullstack & Mobile passionnée, je conçois des applications web et mobiles performantes.
+Solide expérience en intégration continue et architectures API, j'allie rigueur technique et agilité pour livrer des solutions évolutives et centrées sur l'expérience utilisateur.</p>
+
+      <a
+        href="/documents/CV-Mendrika-Ramilison.pdf" 
+        download="CV-Mendrika-Ramilison.pdf"
+        className="download-btn"
+      >
+        Télécharger mon CV
+      </a>
+
     </section>
   );
 }
