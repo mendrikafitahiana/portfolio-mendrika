80128264f975c03833d5919486262b6290062c85
Mendrika Ramilison <mendrika@wylog.com>
2025-05-14 19:53:15 +0300

    contenu projets et compétences

--- portfolio/src/Competences.js
+++ portfolio/src/Competences.js
@@ -2,6 +2,37 @@ export default function Competences() {
   return (
     <section className="home">
       <h1>Compétences</h1>
+      <h3>Frameworks Backend</h3>
+      <ul>
+        <li>Symfony</li>
+        <li>CodeIgniter</li>
+        <li>Nodejs/Express</li>
+        <li>Spring boot</li>
+      </ul>
+
+      <h3>Frameworks Frontend</h3>
+      <ul>
+        <li>React</li>
+        <li>Angular</li>
+        <li>Ionic</li>
+      </ul>
+
+      <h3>Base de données</h3>
+      <ul>
+        <li>MySQL</li>
+        <li>PostgreSQL</li>
+        <li>SQL Server</li>
+        <li>MongoDB</li>
+      </ul>
+
+      <h3>Outils & DevOps</h3>
+      <ul>
+        <li>Github</li>
+        <li>Gitlab</li>
+        <li>Postman</li>
+        <li>Docker</li>
+      </ul>
+
     </section>
   );
 }
--- portfolio/src/Projets.js
+++ portfolio/src/Projets.js
@@ -2,6 +2,27 @@ export default function Projets() {
   return (
     <section className="home">
       <h1>Projets</h1>
+
+      <h3>Wylo-congés</h3>
+      <ul>
+        <li>Type : application web</li>
+        <li>Description : Application interne pour la gestion de congés et d'absences au sein de l'entreprise.</li>
+        <li>Technologies : Symfony | React | MySQL | GitLab</li>
+      </ul>
+
+      <h3>Moov</h3>
+      <ul>
+        <li>Type : application mobile</li>
+        <li>Description : Application mobile pour la gestion et la réservation d'un VTC.</li>
+        <li>Technologies : Node.js/Express | Ionic/React | PostgreSQL | GitHub</li>
+      </ul>
+
+      <h3>Devinsider</h3>
+      <ul>
+        <li>Type : application web</li>
+        <li>Description : Application pour mettre en place des relations entre les profils ISV/Investisseurs/OEM.</li>
+        <li>Technologies : Symfony | React | MySQL | Docker | GitLab</li>
+      </ul>
     </section>
   );
 }
