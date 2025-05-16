34bc618120110b1aabf25d327e93a6f4aefbf180
Mendrika Ramilison <mendrika@wylog.com>
2025-05-16 23:44:40 +0300

    Projets

--- portfolio/src/Projets.js
+++ portfolio/src/Projets.js
@@ -1,7 +1,88 @@
 export default function Projets() {
   return (
-    <section className="home">
-      <h1>Projets</h1>
+    <>
+    <section class="s-intro target-section" id="projets">
+
+      <div class="row intro-content wide">
+
+        <div class="column">
+            <h1 class="text-huge-title">
+              Projets
+            </h1>
+        </div>
+
+        <ul class="intro-social">
+          <li><a href="#0">Email</a></li>
+          <li><a href="#0">Github</a></li>
+          <li><a href="#0">Linkedin</a></li>
+        </ul>
+
+      </div>
+
+    </section>
+
+    <div class="row works-portfolio">
+
+      <div class="column lg-12" data-animate-block>
+
+        <ul class="folio-list row block-lg-one-half block-stack-on-1000">
+
+          <li class="folio-list__item column" data-animate-el>
+            <a class="folio-list__item-link" href="#modal-01">
+                <div class="folio-list__item-pic">
+                    <img src="images/portfolio/fuji.jpg" srcset="images/portfolio/fuji.jpg 1x, images/portfolio/fuji@2x.jpg 2x"  alt=""></img>
+                </div>
+                
+                <div class="folio-list__item-text">
+                    <div class="folio-list__item-cat">
+                        siteweb
+                    </div>
+                    <div class="folio-list__item-title">
+                        Devinsider
+                    </div>
+                </div>
+            </a>
+          </li>
+
+          <li class="folio-list__item column" data-animate-el>
+            <a class="folio-list__item-link" href="#modal-01">
+                <div class="folio-list__item-pic">
+                    <img src="images/portfolio/fuji.jpg" srcset="images/portfolio/fuji.jpg 1x, images/portfolio/fuji@2x.jpg 2x"  alt=""></img>
+                </div>
+                
+                <div class="folio-list__item-text">
+                    <div class="folio-list__item-cat">
+                        mobile
+                    </div>
+                    <div class="folio-list__item-title">
+                        Moov
+                    </div>
+                </div>
+            </a>
+          </li>
+
+          <li class="folio-list__item column" data-animate-el>
+            <a class="folio-list__item-link" href="#modal-01">
+                <div class="folio-list__item-pic">
+                    <img src="images/portfolio/fuji.jpg" srcset="images/portfolio/fuji.jpg 1x, images/portfolio/fuji@2x.jpg 2x"  alt=""></img>
+                </div>
+                
+                <div class="folio-list__item-text">
+                    <div class="folio-list__item-cat">
+                        siteweb
+                    </div>
+                    <div class="folio-list__item-title">
+                        Wylo-congés
+                    </div>
+                </div>
+            </a>
+          </li>
+
+        </ul>
+
+      </div>
+
+    </div>
 
       <h3>Wylo-congés</h3>
       <ul>
@@ -23,6 +104,6 @@ export default function Projets() {
         <li>Description : Application pour mettre en place des relations entre les profils ISV/Investisseurs/OEM.</li>
         <li>Technologies : Symfony | React | MySQL | Docker | GitLab</li>
       </ul>
-    </section>
+    </>
   );
 }
